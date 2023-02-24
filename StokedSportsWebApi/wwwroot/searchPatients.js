4b96cfc7588d05f1cf2d21dddbc918e9e3b9da62
nickywilliams114 <111191470+nickywilliams114@users.noreply.github.com>
2023-02-23 19:40:00 -0700

    checking in changes to work in another branch

---
+++ StokedSportsWebApi/wwwroot/searchPatients.js
@@ -0,0 +1,12 @@
+﻿button.addEventListener('click', (event) => {
+    search()
+};
+
+function search(event) {
+    let firstName = getElementById("firstName").value;
+    let lastName = getElementById("lastName").value;
+    if ("firstName.value.length > 0") {
+        return 
+    }
+
+}
